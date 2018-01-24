de3d19f62e7d189a607f813e93472ca814fff75a
Sam Galizia <samgalizia@gmail.com>
2018-01-24 11:25:26 -0800

    Login & Register Routes

---
+++ routes/login_register_routes.js
@@ -0,0 +1,124 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+
+const User = require('../models/user');
+
+router.use(bodyParser.json());
+
+// POST /register
+router.post('/register', (req, res) => {
+	let username = req.body.username;
+	let password = req.body.password;
+	let email = req.body.email;
+
+	// Validate no fields are empty
+	if(!username || !password || !email) {
+		return res.status(400)
+							.send(JSON.stringify({ 
+								status: 'Failed', 
+								message: 'Cannot have empty fields'
+							}));
+	}
+
+	// Check whether a user already exists with email or username
+	User.findOne({
+		$or: [{email: email},
+					{username: username}]
+	})
+	.then( user => {
+		// If user is nil, continue with creation
+		if(!user) {
+			let user = new User(req.body);
+			user.save();
+
+			// Return the created user
+			res.status(201)
+				 .send(JSON.stringify({
+				 	status: 'Success',
+				 	message: `${username} was created.`
+				 }));
+		}
+		else {
+			res.status(400)
+				 .send(JSON.stringify({
+				 	status: 'Failed',
+				 	message: 'A user already exists with that username or email address.'
+				 }));
+		}
+	})
+	.catch( err => {
+		res.status(400)
+			 .send(JSON.stringify({
+			 	status: 'Failed',
+			 	message: `Error: ${err}`
+			 }));
+	});
+});
+
+// POST /login
+router.post('/login', (req, res) => {
+	let email = req.body.email;
+	let password = req.body.password;
+
+	// Validate there are no empty fields
+	if(!email || !password) {
+		return res.status(400)
+							.send(JSON.stringify({
+								status: 'Failed',
+								message: 'Cannot have empty fields.'
+							}));
+	}
+
+	User.findOne({
+		email: email
+	})
+	.select('+password')
+	.then(user => {
+		if(!user) {
+			// Unable to find the user
+			return res.status(400)
+								.send(JSON.stringify({
+									status: 'Failed',
+									message: 'Invalid credentials. Please check credentials and try again.'
+								}));
+		}
+
+		// Check that the password matches
+		user.comparePassword(password, (err, isMatch) => {
+			if(err) {
+				// Log internal error to console
+				console.log(`Internal Error: ${err}`);
+
+				return res.status(500)
+									.send(JSON.stringify({
+										status: 'Failed',
+										message: '500 Internal Server Error'
+									}));
+			}
+
+			// Validate Credentials
+			if(isMatch) {
+				// Generate JWT token and return it
+				let token = jwt.sign({ _id: user._id, username: user.username}, process.env.SECRET);
+				return res.status(200)
+									.send(JSON.stringify({
+										status: 'Success',
+										message: `${user.username} successfully logged in.`,
+										token: token
+									}));
+			}
+			else {
+				// Password did not match (aka. Invalid Credentials)
+				return res.status(400)
+									.send(JSON.stringify({
+										status: 'Failed',
+										message: 'Invalid credentials. Please check credentials and try again.'
+									}));
+			}
+		});
+	});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+const users = require('./routes/user_routes');
+const login_register = require('./routes/login_register_routes');
+
 // Used for setting up environment variables in Development environment
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +12,27 @@ const PORT = process.env.PORT || 3000;
 // Database Setup - Mongoose
 mongoose.Promise = global.Promise; // Maybe use Bluebird?
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/circuit-studio-api');
+
+// Setup Unauthenticated Routes
+// GET /components
+app.use('/auth', login_register);
+// app.use('/components', components);
+
+// Authentication Middleware
+app.use((req, res, next) => {
+	let authToken = req.get('Authorization');
+
+	if(!authToken) {
+		return res.status(401)
+							.send(JSON.stringify({
+								status: 'Failed',
+								message: 'Unauthorized access. Please check that you are logged in and try again.'
+							}));
+	}
+});
+
+// Setup Authenticated Routes
+app.use('/users', users);
 
 // Start the app and listen on PORT
 app.listen(PORT, () => 
